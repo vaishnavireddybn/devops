12bbca25f9accda1125b8f047ee849e34da0d7c4
vaishnavi <vaishnavi.reddy.bn@gmail.com>
2025-05-30 18:09:33 +0530

    added 10th files

---
+++ src/main/java/p1/mvncmd/example/App.java
@@ -0,0 +1,13 @@
+package p1.mvncmd.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
